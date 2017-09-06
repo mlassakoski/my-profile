a8d862764cedeb3d0bfe3a821c3c4d216ba0fc1b
MAURICIO LASSAKOSKI <mauriciolassakoski@gmail.com>
2017-09-05 23:03:17 -0300

    add menu lis

--- src/react-components/Menu.js
+++ src/react-components/Menu.js
@@ -19,13 +19,23 @@ class Menu extends Component {
                         <a href="#" data-activates="menu-mobile" className="button-collapse">
                             <i onClick={this.handleClick} className="material-icons">menu</i>
                         </a>
-                        <ul className="right hide-on-med-and-down">
+                        <div className="ul-center">
+                            <ul className="center hide-on-med-and-down">
+                                <li><a href='#' target="_blank"><i className="left material-icons">home</i><span>Home</span></a></li>
+                                <li><a href='#' target="_blank"><i className="left material-icons">person</i><span>About me</span></a></li>
+                                <li><a href='#' target="_blank"><i className="left material-icons">code</i><span>Skills</span></a></li>
+                                <li><a href='#' target="_blank"><i className="left material-icons">mail</i><span>Contact</span></a></li>
+                            </ul>
+                        </div>
+                        <ul className="right">
                             <li><a href='https://www.linkedin.com/in/mauriciolassakoski' target="_blank"><i className='fa fa-linkedin fa-2x'></i></a></li>
                             <li><a href='https://github.com/mlassakoski' target="_blank"><i className='fa fa-github fa-2x'></i></a></li>
                         </ul>
                         <ul className="side-nav" id="menu-mobile">
-                            {/* <li><a href='sass.html'><i className='fa fa-linkedin fa-2x'></i></a></li>
-							<li><a href='collapsible.html'><i className='fa fa-github fa-2x'></i></a></li> */}
+                            <li><a href='#' target="_blank"><i className="left material-icons">home</i><span>Home</span></a></li>
+                            <li><a href='#' target="_blank"><i className="left material-icons">person</i><span>About me</span></a></li>
+                            <li><a href='#' target="_blank"><i className="left material-icons">code</i><span>Skills</span></a></li>
+                            <li><a href='#' target="_blank"><i className="left material-icons">mail</i><span>Contact</span></a></li>
                         </ul>
                     </div>
                 </nav>
