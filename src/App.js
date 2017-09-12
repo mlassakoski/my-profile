2158f7623fa085ab9860cf413555b4b3f7923502
MAURICIO LASSAKOSKI <mauriciolassakoski@gmail.com>
2017-09-12 19:48:32 -0300

    adjusted css and layout

--- src/App.js
+++ src/App.js
@@ -12,16 +12,18 @@ class App extends Component {
 		return (
 			<div className='App'>
 
-				<Particles style={{
-					position: 'absolute',
-					top: 0,
-					left: 0,
-					width: '100%',
-					height: '100%'
-				}}
-					params={particleConfig}
-				/>
 
+				<div className='particles'>
+					<Particles style={{
+						position: 'absolute',
+						top: 0,
+						left: 0,
+						width: '100%',
+						height: '100%'
+					}}
+						params={particleConfig}
+					/>
+				</div>
 				<Menu />
 				<Main />
 
