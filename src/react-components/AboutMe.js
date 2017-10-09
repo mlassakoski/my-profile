a083a86c1941d9839028b9b3247983c343b6a212
MAURICIO LASSAKOSKI <mauriciolassakoski@gmail.com>
2017-10-08 21:14:47 -0300

    added panel body into aboutMe component

--- src/react-components/AboutMe.js
+++ src/react-components/AboutMe.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import img from '../img/mlassakoski.png';
+import '../css/AboutMe.css';
+import '../css/Styles.css';
 
 class AboutMe extends Component {
 
     render() {
         return (
-            <div>
-                    about me
-
+            <div className="panel-body">
+                <div className="avatar-box">
+                    <img src={img} alt="" className="circle" />
+                </div>
             </div>
         );
     }
