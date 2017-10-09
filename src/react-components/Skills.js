faafb578ef5c5b379353f7141dbd8f3faed09dea
MAURICIO LASSAKOSKI <mauriciolassakoski@gmail.com>
2017-10-08 21:15:14 -0300

    created skills wordCloud

--- src/react-components/Skills.js
+++ src/react-components/Skills.js
@@ -1,10 +1,58 @@
 import React, { Component } from 'react';
+import WordCloud from 'react-d3-cloud';
+import '../css/Skills.css';
+import skills from '../utils/skills.json';
+import pokemons from '../utils/pokemons.json';
+
+const fontSizeMapper = word => Math.log2(word.value) * 5;
+const rotate = word => word.value % 10;
 
 class Skills extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            pokemonActive: false,
+            fixedShake: true
+        };
+    }
+
+    componentDidMount() {
+        const data = skills.concat(pokemons);
+
+        this.setState({
+            data: data
+        })
+    }
+
+    removePokemons() {
+        let data;
+        const pokemonActive = this.state.pokemonActive
+
+        if (pokemonActive) {
+            data = skills.concat(pokemons);
+        } else {
+            data = skills;
+        }
+
+        this.setState({
+            data: data,
+            pokemonActive: !pokemonActive
+        })
+    }
+
     render() {
         return (
-            <div>
-                skills
+            <div className="panel-body">
+                <a className="waves-effect waves-light btn" onClick={this.removePokemons.bind(this)}>Remove Pokémons</a>
+
+                <WordCloud
+                    data={this.state.data}
+                    fontSizeMapper={fontSizeMapper}
+                    rotate={rotate}
+                    padding={10}
+                />
             </div>
         );
     }
