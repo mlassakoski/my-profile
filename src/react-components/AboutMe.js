5a0287a12e4cd695ff86951d3bbfffa5d96c53bc
MAURICIO LASSAKOSKI <mauriciolassakoski@gmail.com>
2017-10-28 13:40:19 -0200

    create my description box

--- src/react-components/AboutMe.js
+++ src/react-components/AboutMe.js
@@ -9,7 +9,28 @@ class AboutMe extends Component {
         return (
             <div className="panel-body">
                 <div className="avatar-box">
-                    <img src={img} alt="" className="circle" />
+
+                    <div className="row">
+                        <div className="col s4">
+                            <img src={img} alt="" className="circle" />
+                        </div>
+                        <div className="col s8">
+                            <div className="description-box">
+                               <p>
+                                    I have been working with Java since 2014. Currently I working focussed on frontend development. I love beauty and clean code,
+                                    also love to lean new technologies.
+                               </p>
+                                <p>
+                                    Java: Java Language and Platform, HTML, XHTML, CSS, Maven, JUnit, Mockito, Liquibase, Hibernate, JPA 2.0, Spring, WebServices and Jersey
+
+                                    JavaScript: ES6, AngulasJs, Angular(2x, 4x), ReactJs, Jasmine, Mocha, node, TypeScript
+
+                                    DataBases: Oracle, Hsql, H2, Mysql, Mongo
+                                </p>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
