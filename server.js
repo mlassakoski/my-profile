612128c95861c452e8cb89338213674c630c422d
MAURICIO LASSAKOSKI <mauriciolassakoski@gmail.com>
2017-12-24 15:06:13 -0200

    serverjs

---
+++ server.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const app = express();
+
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+app.use(forceSSL());
+app.use(express.static(__dirname + '/dist'));
+app.listen(process.env.PORT || 8080);
+
+const path = require('path');
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
