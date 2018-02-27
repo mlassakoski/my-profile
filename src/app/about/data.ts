ad252158a4a4eeb81ee15a3e11b326cc4c3276f7
MAURICIO LASSAKOSKI <mauriciolassakoski@gmail.com>
2018-02-27 20:48:23 -0300

    update info

--- src/app/about/data.ts
+++ src/app/about/data.ts
@@ -1,16 +1,44 @@
 export const skills = [
   {
     name: 'Team Work',
+    value: '80%',
+    style: {
+      width: '80%',
+    }
+  },
+  {
+    name: 'Analytical Thinking',
     value: '75%',
     style: {
       width: '75%',
     }
   },
   {
-    name: 'Team Work',
+    name: 'Coffee drinking',
+    value: '95%',
+    style: {
+      width: '95%',
+    }
+  },
+  {
+    name: 'Commitment',
+    value: '80%',
+    style: {
+      width: '80%',
+    }
+  },
+  {
+    name: 'Proactive',
     value: '85%',
     style: {
       width: '85%',
+    }
+  },
+  {
+    name: 'Creativity',
+    value: '80%',
+    style: {
+      width: '80%',
     }
   }
 ];
